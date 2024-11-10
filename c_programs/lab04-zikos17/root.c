12389dbebe4511905a2e9c805af97aa019f4fdfc
zikos17 <sdi2400052@di.uoa.gr>
2024-11-10 22:59:10 +0200

    egasia #0

--- c_programs/lab04-zikos17/root.c
+++ c_programs/lab04-zikos17/root.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <time.h>
 
 int main() {
     double a,b,c;
-    srand(time(NULL));
+    srand(42);
     a=(1.0*rand())/RAND_MAX;
     b=((double)rand())/RAND_MAX;
     c=rand()/(double)RAND_MAX;
-   double det=b*b - 4*a*c ;
-    if (det>0) {
-    double x1 = (-b + sqrt(det))/ (2*a);
-    double x2 = (-b - sqrt(det))/(2*a);
-    printf("x1: %lf x2: %lf\n",x1 ,x2);
-     } else if (!(det==0)){
-        double x= -b /(2*a);
-        printf("double root: %lf\n", x);
-     }
 
     printf("%lf\n",a);
     printf("%lf\n",b);
